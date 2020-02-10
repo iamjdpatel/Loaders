4d7ddf4c1708fce744f693575fbd0d0df9cba406
JD Patel <jay@citrintech.com>
2020-02-10 20:26:31 +0530

    Initial Commit

---
+++ Loader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Loader
+//
+//  Created by JD Patel on 10/02/20.
+//  Copyright © 2020 JD. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
